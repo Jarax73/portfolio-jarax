6c19e3b8c2a984fa1227447536bf9650199e7892
Jarax73 <jaraxkapila73@gmail.com>
2024-11-01 16:31:57 +0100

    Change url for quiz

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -55,7 +55,7 @@ export default function Projects() {
                 <h3>
                   <Link
                     className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                    href="https://quiz-app-jakaps.kda-dev-web-2022-classe-a.repl.co"
+                    href="https://jarax73.github.io/quiz-javascript/"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -77,7 +77,7 @@ export default function Projects() {
               />
             </div>
           </li>
-          <li className="mb-12">
+          {/* <li className="mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
@@ -104,7 +104,7 @@ export default function Projects() {
                 alt="Music app"
               />
             </div>
-          </li>
+          </li> */}
         </ul>
       </div>
     </section>
